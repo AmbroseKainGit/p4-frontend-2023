b967c939d8273a631886a1841700dbc0349e01ec
Cristian Ceballos <cmceballos88@gmail.com>
2023-06-24 15:14:52 +0200

    Complete card design for details

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -18,7 +18,7 @@ const Detail = () => {
 
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="flex flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col max-w-2xl min-w-[40rem] p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-[#C97D60] dark:border-[#321325]">
         <h5 className="text-center capitalize mb-2 text-4xl font-semibold tracking-tight text-gray-900 dark:text-white">
           {pokemon?.name}
         </h5>
@@ -29,28 +29,48 @@ const Detail = () => {
             transition: "background-image 0.3s ease"
           }}
         ></div>
-         <h5 className="text-center capitalize mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          Moves
-        </h5>
-        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
-          Go to this step by step guideline process on how to certify for your
-          weekly benefits:
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center text-blue-600 hover:underline"
-        >
-          See our guideline
-          <svg
-            className="w-5 h-5 ml-2"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-          </svg>
-        </a>
+        <div className="text-white grid grid-cols-3 gap-4 w-full">
+          <div className="flex flex-col items-center">
+            <h5 className="text-center capitalize mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              Moves
+            </h5>
+            <ol className="dark:text-white capitalize ">
+              {pokemon?.moves.map(
+                (move, index) =>
+                  index <= 5 && (
+                    <li key={move.move.name} className="list-disc">
+                      {move.move.name}
+                    </li>
+                  )
+              )}
+            </ol>
+          </div>
+          <div className="flex flex-col items-center">
+            <h5 className="text-center capitalize mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              stats
+            </h5>
+            <ol className="dark:text-white capitalize">
+              {pokemon?.stats.map((stat, index) => (
+                <li key={stat.stat.name} className="list-disc">
+                  {stat.stat.name} : {stat?.base_stat}
+                </li>
+              ))}
+            </ol>
+          </div>
+          <div className="flex flex-col items-center">
+            <h5 className="text-center capitalize mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              types
+            </h5>
+            <ol className="dark:text-white capitalize">
+              {pokemon?.types.map((type, index) => (
+                <li className="list-disc" key={type?.type.name}>
+                  {type?.type.name}
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   );
