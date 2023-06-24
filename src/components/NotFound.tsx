79c31a8cc83237538c9388679935cde8a41cf477
Cristian Ceballos <cmceballos88@gmail.com>
2023-06-24 12:58:15 +0200

    Add 404 page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center flex-col bg-[#C97D60]">
+      <h1 className="text-[#F8F4E3] text-4xl">404 PAGE NOT FOUND</h1>
+      <img className="w-40 h-40" src="/src/assets/missigno.png" />
+    </div>
+  );
+};
+
+export default NotFound;
--- src/router/RouterProvider.tsx
+++ src/router/RouterProvider.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
+import NotFound from "../components/NotFound";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
